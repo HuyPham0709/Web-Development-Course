59f5f19feded0177cc426cc6fb4c1d3c80c18b2b
hieutrung10042 <txxh1004@gmail.com>
2026-06-03 05:29:12 +0700

    fix: allow vercel origin in cors configuration

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,14 @@ const PORT = process.env.PORT || 5000;
 // ─────────────────────────────────────────────────────────────
 // 1. GLOBAL MIDDLEWARES (Cấu hình toàn cục)
 // ─────────────────────────────────────────────────────────────
-const cors = require('cors');
 
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174","https://web-development-course-steel.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
